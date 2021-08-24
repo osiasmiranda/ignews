1970f5ef163b60e95a1b83faa3f2194e9b5acc63
Osias Miranda <osiasmiranda@gmail.com>
2021-08-24 17:34:13 -0400

    configurando fontes externas

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,20 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;900&display=swap"
+            rel="stylesheet"
+          />
+
           <title>ig.News</title>
         </Head>
         <body>
