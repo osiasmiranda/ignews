2191fbd6b3f6e3d62846b42dbcf3ddc4f464f68c
Osias Miranda <osiasmiranda@gmail.com>
2021-08-24 21:25:42 -0400

    configurando o prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import styles from "../../styles/Home.module.scss";
-import Head from "next/head";
+import styles from '../../styles/Home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
